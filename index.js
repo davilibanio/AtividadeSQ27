0dad60a34993bbd729de49d5eb522b4e450d71aa
davilibanio <94376404+davilibanio@users.noreply.github.com>
2022-02-07 12:17:07 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const escrever = ()=>{
+
+    alert('somente números ')
+
+    const arg = prompt('digite um número')
+
+    for(let i=1; i <= arg; i++){
+        document.getElementById('resposta').innerHTML += i + " ";
+
+    }
+
+        
+   
+
+}
